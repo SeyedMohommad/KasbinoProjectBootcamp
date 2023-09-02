677bc2319fe9a3c7e7dc26a79d8fed5d5688a3dc
ashkan <a.amin771116@yahoo.com>
2023-09-02 19:24:35 +0330

    simple websocket chat project with spring boot

---
+++ demo/src/main/java/top/ashkanamin/demo/chat/MessageType.java
@@ -0,0 +1,7 @@
+package top.ashkanamin.demo.chat;
+
+public enum MessageType {
+    CHAT,
+    JOIN,
+    LEAVE
+}
---
+++ demo/src/test/java/top/ashkanamin/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package top.ashkanamin.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
